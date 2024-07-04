5e652a70c27c44177160a25307ef51c4e1aea2d4
Markislost <140057175+Markislost@users.noreply.github.com>
2024-07-04 16:38:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const showBtn = document.querySelector('.btn-bars');
-const closeBtn= document.querySelector('.btn-close');
-const navMenu = document.querySelector('.navbar-collapse');
+document.addEventListener('DOMContentLoaded', function() {
+    var videoPlayer = document.getElementById('videoPlayer');
+    var iframe = document.getElementById('myVideo');
+    var playButtons = document.querySelectorAll('.play-btn');
+    var showBtn = document.querySelector('.btn-bars');
+    var closeBtn = document.querySelector('.btn-close');
+    var navMenu = document.querySelector('.navbar-collapse');
 
-showBtn.addEventListener('click',()=>{
-    navMenu.classList.add('showMenu');
-});
+    playButtons.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            var videoUrl = btn.getAttribute('data-video-url');
+            playVideo(videoUrl);
+        });
+    });
 
-closeBtn.addEventListener('click',()=>{
-    navMenu.classList.remove('showMenu');
+    showBtn.addEventListener('click', function() {
+        navMenu.classList.add('showMenu');
+    });
+
+    closeBtn.addEventListener('click', function() {
+        navMenu.classList.remove('showMenu');
+    });
+
+    function playVideo(videoUrl) {
+        iframe.src = videoUrl + '&autoplay=1';
+        videoPlayer.style.display = 'block';
+    }
+
+    window.stopVideo = function() {
+        iframe.src = '';
+        videoPlayer.style.display = 'none';
+    };
 });
- 
